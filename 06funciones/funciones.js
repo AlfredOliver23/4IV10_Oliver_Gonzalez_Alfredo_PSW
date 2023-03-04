4fc17a8f67006582724bcdba5f30fb4e8bd9c2c3
AlfredOliver23 <aoliverg2100@alumno.ipn.mx>
2023-03-04 17:12:12 -0600

    Actualizacion

---
+++ 06funciones/funciones.js
@@ -0,0 +1,18 @@
+function sumar(n1,n2){
+    return n1 + n2
+}
+
+console.log(`la syna de (3,4):`+ $(sumar))
+una funcion flecha tiene como estrictira cadena --> IdleDeadline, la clase , name,atributo
+*/ 
+const sunar funcion flecha = (n1,n2)=>n1 +n2;
+
+console.log(la suma de (5,6)):
+$(sumarfuncionflecha)
+
+//con un gflor normal
+for (let indice = 0; indice)
+FileSystemDirectoryEntry()
+for(cinst indice in razasdeperros){
+    console.log(razas de perros)
+}
